c58841bf258fb791d90a786652391dbdab94089e
muke101 <muke101@protonmail.com>
2019-12-23 22:43:17 +0000

    fixed === test case

--- lexer.scala
+++ lexer.scala
@@ -5,11 +5,11 @@ import java.util.regex.Pattern
 object Main {
   def main(args: Array[String]) = {
     val input = Source.fromFile(args(0)).mkString
-    val isNumber = "((-)?[0-9]+)"
+    val isNumber = "(-?[0-9]+)"
     val isID = "([a-z]+[a-zA-Z0-9_]*)"
     val isKeyword = "([def|if|then|else|skip|while|do|repeat|until|break|and|continue])"
     val isString = "([0-9a-zA-Z]+)"
-    val isOp = "([;|\\(|\\)|==|=|<=|>=|<|>|,|{|}|:=|+|*|-|/])"
+    val isOp = "(;|\\(|\\)|==|=|<=|>=|<|>|,|\\{|\\}|:=|\\+|\\*|-|/)"
     val R = List(isString,isNumber,isOp).mkString("|").r
     val tokens = List()
     for (pattern <- R.findAllMatchIn(input))  {
